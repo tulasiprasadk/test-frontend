9450e2300580a998cfdbaf5eb0606e0aa7fb7cda
tulasiprasadk <tulasiprasadk@gmail.com>
2025-12-28 11:32:33 +0530

    fix: rename auth0-provider.js to .jsx for Vite JSX support

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import axios from "axios";
 import { AuthProvider } from "./context/AuthContext";
-import AuthProviderWrapper from "./auth0-provider";
+import AuthProviderWrapper from "./auth0-provider.jsx";
 
 axios.defaults.withCredentials = true;
 const baseName = import.meta.env.VITE_BASE_URL || "/";
